c10e4119ca87b06119b28c072dedcd16d41e9c17
bksg <bskippinggill@gmail.com>
2014-09-05 18:27:20 -0500

    made an age check function

---
+++ ch5.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def ageCheck(age):
+    if age > 20:
+        print("you are too old")
+    elif age<1:
+        print("you are not born yet")
+    elif age <15:
+        print ('you are too young')
+    else:
+        print("you are the right age")
